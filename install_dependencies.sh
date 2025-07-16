094b399da429b759f9b04febe3bacd277753b7cc
patrickt <patrickt@node0.dynamic-index-2.e6897-pg0.utah.cloudlab.us>
2025-07-15 19:17:10 -0600

    Update script name

---
+++ install_dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+sudo apt install cmake
+sudo apt install libomp-dev
+sudo apt install libgoogle-perftools-dev
+sudo apt install libboost-program-options1.74.0
+sudo apt install intel-mkl
+sudo apt install libboost-all-dev
+sudo apt install libaio-dev
+
+sudo apt install python3-pip
+
+pip install numpy
+pip install matplotlib
